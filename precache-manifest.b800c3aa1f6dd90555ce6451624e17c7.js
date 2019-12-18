783414772cdaa5d8b83f81b539e16c7b869520a4
Manikanth13 <mani.dindigala29@gmail.com>
2019-12-18 15:17:59 +0100

    Updates

---
+++ precache-manifest.b800c3aa1f6dd90555ce6451624e17c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56135a358fa7809edc2d16be5d9b84f9",
+    "url": "/optile-coding-task/index.html"
+  },
+  {
+    "revision": "8b88d0bb7eb0d29831b5",
+    "url": "/optile-coding-task/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "497b08b525bae050b0d1",
+    "url": "/optile-coding-task/static/js/2.e5224695.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/optile-coding-task/static/js/2.e5224695.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b88d0bb7eb0d29831b5",
+    "url": "/optile-coding-task/static/js/main.1845d8ad.chunk.js"
+  },
+  {
+    "revision": "fd33f810d60734e72037",
+    "url": "/optile-coding-task/static/js/runtime-main.0e554471.js"
+  }
+]);
